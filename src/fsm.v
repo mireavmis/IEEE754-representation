6a77d4e1dcb570dabad889e5a90ed28050567778
Evgenii <gostevevg0@gmail.com>
2023-04-03 18:27:39 +0300

    tested fsm added

--- src/fsm.v
+++ src/fsm.v
@@ -15,7 +15,7 @@ parameter S0 = 0,
           S5 = 5,
           S6 = 6;
 
-reg [3:0] state, new_state;
+reg [3:0] state;
 reg [15:0] REG_IN, REG_RES;
 reg [16:0] REG_TMP;
 reg REG_ERROR;
@@ -25,7 +25,6 @@ integer i, j, point;
 
 initial begin
     state     = S0;
-    new_state = 0;
     REG_RES   = 0;
     REG_IN    = 0;
     REG_TMP   = 0;
@@ -42,8 +41,6 @@ always@(posedge clk) begin
     
     if (reset)
         state <= S0;
-    else
-        state <= new_state;
 end
 
 always@(posedge clk) begin
@@ -57,69 +54,74 @@ always@(posedge clk) begin
             REG_SIGN  <= 0;
             REG_ERROR <= 0;
             
-            new_state <= S1;
+            state <= S1;
         end
         S1: begin
             if (R_I) begin
                 REG_IN <= dataIn;
 
-                new_state <= S2;
+                state <= S2;
             end
         end
         S2: begin
-            REG_SIGN = REG_IN[15];
+            REG_SIGN = REG_IN[15-j-1];
             REG_TMP  <= REG_IN;
 
             if (REG_SIGN)
-                new_state <= S3;
+                state <= S3;
             else
-                new_state <= S4;
+                state <= S4;
         end
         S3: begin
             REG_TMP     = ~REG_TMP;
             REG_TMP[16] = 0;
             REG_TMP     = REG_TMP + 1;
 
-            new_state <= S4;
+            state <= S4;
         end
         S4: begin
             if (REG_TMP > 2048)
-                new_state <= S5;
+                state <= S5;
             else
-                new_state <= S6;
+                state <= S6;
         end
         S5: begin
             REG_ERROR <= 1;
             
-            new_state <= S0;
+            state <= S0;
         end
         S6: begin
-            for (i = 0; i < 11; i = i + 1) begin
-                if (REG_TMP[i] == 1)
-                    point = i;
-                else
-                    point = point;
+            if (REG_IN == 0) begin
+                REG_RES <= 0;
             end
+            else begin
+                for (i = 0; i < 11; i = i + 1) begin
+                    if (REG_TMP[i] == 1)
+                        point = i;
+                    else
+                        point = point;
+                end
 
-            REG_RES[15] <= REG_SIGN;
-            REG_RES[14:10] <= 15 + point;
-            case(point)
-                0:  for (j = 0; j < 2; j = j + 1)  REG_RES[9-j] <= REG_TMP[j];
-                1:  for (j = 0; j < 2; j = j + 1)  REG_RES[9-j] <= REG_TMP[j];
-                2:  for (j = 0; j < 2; j = j + 1)  REG_RES[9-j] <= REG_TMP[j];
-                3:  for (j = 0; j < 3; j = j + 1)  REG_RES[9-j] <= REG_TMP[j];
-                4:  for (j = 0; j < 4; j = j + 1)  REG_RES[9-j] <= REG_TMP[j];
-                5:  for (j = 0; j < 5; j = j + 1)  REG_RES[9-j] <= REG_TMP[j];
-                6:  for (j = 0; j < 6; j = j + 1)  REG_RES[9-j] <= REG_TMP[j];
-                7:  for (j = 0; j < 7; j = j + 1)  REG_RES[9-j] <= REG_TMP[j];
-                8:  for (j = 0; j < 8; j = j + 1)  REG_RES[9-j] <= REG_TMP[j];
-                9:  for (j = 0; j < 9; j = j + 1)  REG_RES[9-j] <= REG_TMP[j];
-                10: for (j = 0; j < 10; j = j + 1) REG_RES[9-j] <= REG_TMP[j];
-            endcase
+                REG_RES[15] <= REG_SIGN;
+                REG_RES[14:10] <= 15 + point;
+                case(point)
+                    0:  for (j = 0; j < 0; j = j + 1)  REG_RES[9-j] <= REG_TMP[point-j-1];
+                    1:  for (j = 0; j < 1; j = j + 1)  REG_RES[9-j] <= REG_TMP[point-j-1];
+                    2:  for (j = 0; j < 2; j = j + 1)  REG_RES[9-j] <= REG_TMP[point-j-1];
+                    3:  for (j = 0; j < 3; j = j + 1)  REG_RES[9-j] <= REG_TMP[point-j-1];
+                    4:  for (j = 0; j < 4; j = j + 1)  REG_RES[9-j] <= REG_TMP[point-j-1];
+                    5:  for (j = 0; j < 5; j = j + 1)  REG_RES[9-j] <= REG_TMP[point-j-1];
+                    6:  for (j = 0; j < 6; j = j + 1)  REG_RES[9-j] <= REG_TMP[point-j-1];
+                    7:  for (j = 0; j < 7; j = j + 1)  REG_RES[9-j] <= REG_TMP[point-j-1];
+                    8:  for (j = 0; j < 8; j = j + 1)  REG_RES[9-j] <= REG_TMP[point-j-1];
+                    9:  for (j = 0; j < 9; j = j + 1)  REG_RES[9-j] <= REG_TMP[point-j-1];
+                    10: for (j = 0; j < 10; j = j + 1) REG_RES[9-j] <= REG_TMP[point-j-1];
+                endcase
 
+            end
             R_O <= 1;
 
-            new_state <= S0;
+            state <= S0;
         end
     endcase
 end
